b95a76170d54658d4af767a935ef6c54ee77b66a
Alexander Paschenko <past.first@gmail.com>
2019-07-09 08:23:28 +0300

    finish developing the functionality

--- public/script.js
+++ public/script.js
@@ -13,27 +13,38 @@ function addEnemy() {
     });
 
     document.getElementById("enemies").appendChild(newEnemy);
+
+    setRemButtonVisibility();
 }
 
 function  removeLastEnemy() {
     const enemies = document.getElementById("enemies");
-    enemies.removeChild(enemies.lastChild);
+    if (enemies.childElementCount > 1) {
+        enemies.removeChild(enemies.lastChild);
+    }
+
+    setRemButtonVisibility();
+}
+
+function setRemButtonVisibility() {
+    const numOfEnemies = document.getElementById("enemies").childElementCount;
+    document.getElementById("remove_button").style.display =
+        (numOfEnemies > 1) ? "block" : "none"
 }
 
 function collectData() {
     const data = {
-        firingRange: document.getElementById("firing_range").value,
+        firingRange: parseFloat( document.getElementById("firing_range").value ),
         enemies: []
     };
 
     const enemies = document.getElementById("enemies").children;
     for (let i = 0; i < enemies.length; i++) {
-        const enemy = ["e_name", "e_dist", "e_speed"].reduce(function (acc, item) {
-            acc[item] = document.getElementById(item + "_" + i).value;
-            return acc;
-        }, {});
-
-        data.enemies.push(enemy);
+        data.enemies.push({
+            name: document.getElementById("e_name_" + i).value,
+            distance: parseFloat( document.getElementById("e_dist_" + i).value ),
+            speed: parseFloat( document.getElementById("e_speed_" + i).value )
+        });
     }
 
     return data;
@@ -42,16 +53,84 @@ function collectData() {
 function transferData() {
     const outgoingData = collectData();
 
-    return fetch('/', {
-        method: 'POST',
-        cache: 'no-cache',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(outgoingData)
-    })
-    .then(function (response) {
-        console.log(response.json());
+    const isDataValid = validator(outgoingData);
+    showHideError(isDataValid);
+
+    if (isDataValid) {
+        fetch('/', {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(outgoingData)
+        })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function (data) {
+                showResults(data);
+            });
+    }
+}
+
+function showResults(response) {
+    document.getElementById('o_firing_range').innerText = `${response.firingRange}m`;
+
+    const historyNode = document.getElementById('o_history');
+    historyNode.innerHTML = '';
+
+    response.firstRoundHistory.forEach(rowData => {
+        const rowNode = document.getElementById('row_pattern').cloneNode(true);
+
+        rowNode.querySelector(".o_turn_number").textContent = rowData.turn;
+        rowNode.querySelector(".o_name").textContent = rowData.name;
+        rowNode.querySelector(".o_dist").textContent = `${rowData.distance}m`;
+
+        rowNode.style.display = 'block';
+
+        historyNode.appendChild(rowNode);
     });
 
+    document.getElementById('o_status').textContent = response.firstRoundStatus;
+    document.getElementById('o_turns').textContent = response.turns;
+
+    const minNode = document.getElementById('o_minimum');
+    if (response.firstRoundStatus === "WIN") {
+        minNode.style.display = "none";
+    } else {
+        document.getElementById('o_min_fr').textContent = response.minFiringRangeForWin;
+        minNode.style.display = 'block';
+    }
+
+    document.getElementById('output').style.display = 'block';
+}
+
+function showHideError(isDataValid) {
+    const errorNode = document.getElementById('error');
+    errorNode.style.display = isDataValid ? 'none' : 'block';
+}
+
+function validator(data) {
+    if (data.enemies.length === 0) {
+        return false;
+    }
+
+    if ( isNotAPositiveNumber(data.firingRange) ) {
+        return false;
+    }
+
+    let isDataValid = true;
+    for (let i = 0; i < data.enemies.length; i++) {
+        const enemy = data.enemies[i];
+        if ( isNotAPositiveNumber(enemy.distance) || isNotAPositiveNumber(enemy.speed) ) {
+            isDataValid = false;
+            break;
+        }
+    }
+    return isDataValid;
+}
+
+function isNotAPositiveNumber(number) {
+    return isNaN(number) || number <= 0;
 }
